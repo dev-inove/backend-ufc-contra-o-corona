ffc1dfdaaf37eb9cecefc7ac83ed15cc523b7db6
lunebakami <luis.santos.taiba@gmail.com>
2020-06-03 21:58:08 -0300

    user model and migration

--- src/config/database.js
+++ src/config/database.js
@@ -6,6 +6,7 @@ module.exports = {
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  port: 3306,
   define: {
     timestamp: true,
     underscored: true,
